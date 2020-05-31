f0051adc824d71147cb6d6105c6088c0a9c73f4a
lisivka <lisivka@gmail.com>
2020-05-31 11:13:01 +0300

    Add Models Db + Adminka

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def sign_up(request):
+
+    return render(request, 'accounts/sign_up.html')
+
+
+def sign_in(request):
+
+    return render(request, 'accounts/sign_in.html')
+
+
+def sign_out(request):
+
+    return render(request, 'accounts/sign_out.html')
