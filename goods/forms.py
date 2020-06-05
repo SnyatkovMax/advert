f46354df52c65a2baa4ee1840c2d0f414eed2d5f
lisivka <lisivka@gmail.com>
2020-06-05 14:17:35 +0300

    CRUD

--- goods/forms.py
+++ goods/forms.py
@@ -9,3 +9,11 @@ class PostForm(forms.ModelForm):
                   'size', 'photoURL', 'videoURL', 'photo', 'address', 'city', 'country')
 
 
+class PostForm2(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('name', 'description', 'type', 'status', 'location', 'beds', 'baths', 'floors', 'metro', 'area',
+                  'size', 'photoURL', 'videoURL', 'photo', 'address', 'city', 'country')
+
+
+
